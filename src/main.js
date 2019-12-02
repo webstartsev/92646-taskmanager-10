aaee4f9eea7a446dae0ef5c49fdb573037aa6884
Sergey Startsev <sergey.starcev@imeretinka.ru>
2019-12-02 11:29:17 +0300

    update generate filter

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import {createFormTaskEditTemplate} from './components/form-task';
 import {createTaskTemplate} from './components/task';
 import {createLoadMoreBtnTemplate} from './components/load-more-btn';
 import {generateTasks} from './mock/task.js';
+import {generateFilters} from './mock/filter.js';
 
 const TASK_COUNT = 3;
 
@@ -16,14 +17,15 @@ const mainElement = document.querySelector(`.main`);
 const mainControlElement = mainElement.querySelector(`.main__control`);
 
 render(mainControlElement, createMenuTemplate());
-render(mainElement, createFilterTemplate());
+
+const filters = generateFilters();
+render(mainElement, createFilterTemplate(filters));
 render(mainElement, createBoardTemplate());
 
 const boardElement = mainElement.querySelector(`.board`);
 const taskListElement = boardElement.querySelector(`.board__tasks`);
 
 const tasks = generateTasks(TASK_COUNT);
-
 render(taskListElement, createFormTaskEditTemplate(tasks[0]));
 for (let index = 1; index < tasks.length; index++) {
   render(taskListElement, createTaskTemplate(tasks[index]));
