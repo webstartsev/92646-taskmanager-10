12f1348de487e7f5933d4780b63057f155e08fcf
Sergey Startsev <sergey.starcev@imeretinka.ru>
2019-12-02 13:47:10 +0300

    add getFullDate util

--- src/utils.js
+++ src/utils.js
@@ -20,3 +20,11 @@ export const formatTime = (date) => {
 
   return `${hours}:${minutes} ${interval}`;
 };
+
+export const getFullDate = (date = new Date()) => {
+  const dd = String(date.getDate()).padStart(2, `0`);
+  const mm = String(date.getMonth() + 1).padStart(2, `0`);
+  const yyyy = date.getFullYear();
+
+  return `${dd}.${mm}.${yyyy}`;
+};
