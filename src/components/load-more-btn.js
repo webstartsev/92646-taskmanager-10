ed6e5cf3ebd410e74c102cf8fb16e5c44563b4f1
Sergey Startsev <sergey.starcev@imeretinka.ru>
2019-12-03 16:21:59 +0300

    update LoadMoreBtn to class

--- src/components/load-more-btn.js
+++ src/components/load-more-btn.js
@@ -1,5 +1,30 @@
-export const createLoadMoreBtnTemplate = () => {
+import {createElement} from '../utils.js';
+
+const createLoadMoreBtnTemplate = () => {
   return (
     `<button class="load-more" type="button">load more</button>`
   );
 };
+
+
+export default class LoadMoreBtn {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createLoadMoreBtnTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
