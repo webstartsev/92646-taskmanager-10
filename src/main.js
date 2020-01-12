fde0d8a062119a37b05d06f6e328514bfda152e9
Sergey Startsev <sergey@webstartsev.ru>
2020-01-12 14:24:08 +0300

    remove mocks data

--- src/main.js
+++ src/main.js
@@ -3,20 +3,15 @@ import StatisticsComponent from "./components/statistic.js";
 import BoardController from './controllers/board.js';
 import FilterController from "./controllers/filter.js";
 import TasksModel from "./models/tasks.js";
-import {generateTasks} from './mock/task.js';
 
 import {render} from './utils/render.js';
-
-const TASK_COUNT = 22;
 
 const mainElement = document.querySelector(`.main`);
 const mainControlElement = mainElement.querySelector(`.main__control`);
 const menuComponent = new MenuComponent();
 render(mainControlElement, menuComponent);
 
-const tasks = generateTasks(TASK_COUNT);
 const tasksModel = new TasksModel();
-tasksModel.setTasks(tasks);
 
 const filterController = new FilterController(mainElement, tasksModel);
 filterController.render();
