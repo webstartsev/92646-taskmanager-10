f33fb82b8c66068529f7b50daa5888145ab8eab8
Sergey Startsev <sergey@webstartsev.ru>
2020-01-16 15:11:31 +0300

    Privider save data to Store

--- src/api/provider.js
+++ src/api/provider.js
@@ -9,35 +9,61 @@ export default class Provider {
 
   getTasks() {
     if (this._isOnline()) {
-      return this._api.getTasks();
+      return this._api.getTasks()
+        .then((tasks) => {
+          tasks.forEach((task) => {
+            this._store.setItem(task.id, task.toRAW());
+            return task;
+          });
+        });
     }
 
-    return Promise.resolve(Task.parseTasks([]));
+    const storeTasks = Object.values(this._store.getAll());
+
+    return Promise.resolve(Task.parseTasks(storeTasks));
   }
 
   createTask(task) {
     if (this._isOnline()) {
-      return this._api.createTask(task);
+      return this._api.createTask(task)
+        .then((newTask) => {
+          this._store.setItem(newTask.id, newTask.toRAW());
+          return newTask;
+        });
     }
 
     const fakeNewTaskId = nanoid();
     const fakeNewTask = Task.parseTask(Object.assign({}, task.toRAW(), {id: fakeNewTaskId}));
+
+    this._store.setItem(fakeNewTask.id, Object.assign({}, fakeNewTask.toRAW(), {offline: true}));
 
     return Promise.resolve(fakeNewTask);
   }
 
   updateTask(id, task) {
     if (this._isOnline()) {
-      return this._api.updateTask(id, task);
+      return this._api.updateTask(id, task)
+        .then((newItem) => {
+          this._store.setItem(newItem.id, newItem.toRAW());
+          return newItem;
+        });
     }
 
-    return Promise.resolve(task);
+    const fakeUpdateTask = Task.parseTask(Object.assign({}, task.toRAW(), {id}));
+    this._store.setItem(id, Object.assign({}, fakeUpdateTask.toRAW(), {offline: true}));
+
+    return Promise.resolve(fakeUpdateTask);
   }
 
   deleteTask(id) {
     if (this._isOnline()) {
-      return this._api.deleteTask(id);
+      return this._api.deleteTask(id)
+        .then(() => {
+          this._store.removeItem(id);
+        });
     }
+
+    this._store.removeItem(id);
 
     return Promise.resolve();
   }
