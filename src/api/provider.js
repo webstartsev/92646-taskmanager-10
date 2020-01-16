78b5682224b364b0da784e64c0bc058339a2a19e
Sergey Startsev <sergey@webstartsev.ru>
2020-01-16 12:20:17 +0300

    add Provider

---
+++ src/api/provider.js
@@ -0,0 +1,22 @@
+export default class Provider {
+  constructor(api, store) {
+    this._api = api;
+    this._store = store;
+  }
+
+  getTasks() {
+    return this._api.getTasks();
+  }
+
+  createTask(task) {
+    return this._api.createTask(task);
+  }
+
+  updateTask(id, task) {
+    return this._api.updateTask(id, task);
+  }
+
+  deleteTask(id) {
+    return this._api.deleteTask(id);
+  }
+}
